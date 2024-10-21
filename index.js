748b038f48993656499b51d726855cb5f3ff9774
nathidaum <nathidaum@gmail.com>
2024-10-21 15:48:18 +0200

    Finished Lab

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b); // Sort the array after adding the item
+      this.length = this.items.length;   // Update the length property
+    }
 
-  add(item) {}
+  get(pos) {
+    if(pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.at(pos);
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+      return this.items[this.length - 1]; // Return the last item in the sorted array
+  }
 
-  max() {}
+  min() {
+    if(this.length === 0) {
+      throw new Error("Empty SortedList");
+    } 
+    return this.items[0]; // Return the first item in the sorted array
+  }
 
-  min() {}
+  sum() {
+    return this.sum = this.items.reduce((acc, currentNumber) => acc + currentNumber, 0)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if(this.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
